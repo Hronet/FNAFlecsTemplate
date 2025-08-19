5e0f6e6da3b4415ca40099c856fce95c178d213d
Zack <zheckman89@gmail.com>
2025-08-18 20:07:23 -0700

    Initial Commit

---
+++ Default/Default/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Default;
+
+internal class FNAGame : Game
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        using var g = new FNAGame();
+        g.Run();
+    }
+    
+    private FNAGame()
+    {
+        var deviceManager = new GraphicsDeviceManager(this);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        base.Draw(gameTime);
+    }
+}
